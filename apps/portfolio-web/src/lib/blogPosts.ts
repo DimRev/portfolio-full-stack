420ba1d965bdcb0feab125e07c6a298023af76e9
DimRev <dimarev444@gmail.com>
2024-05-28 19:59:37 +0300

    Change image to one hosted in uploadthing and add link in md

--- apps/portfolio-web/src/lib/blogPosts.ts
+++ apps/portfolio-web/src/lib/blogPosts.ts
@@ -5,7 +5,7 @@ export const BLOG_POSTS = [
     title: "Reflections Of a Junior",
     slug: "1-reflections-of-a-junior",
     imgUrl:
-      "https://res.cloudinary.com/practicaldev/image/fetch/s--08z6gIzC--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/l9w3sg7lfal6o0ewhxht.jpg",
+      "https://utfs.io/f/0b4009bd-b77f-40e6-a6ee-b6093ce02e58-4ja9om.webp",
     component: ReflectionsOfAJunior,
   },
 ];
